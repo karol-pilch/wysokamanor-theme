8f022ffcb9758831c807d9cf59f010ca1539aaf6
karol-pilch <karoop@gmail.com>
2016-07-17 14:25:18 +0100

    Working on #7: The JS is mostly done for the image preview.

--- js/image-preview.js
+++ js/image-preview.js
@@ -52,8 +52,87 @@
 			$(container).toggleClass('wm-hover', enable);
 		},
 
+		navTargets: {
+			prev: null,
+			next: null
+		},
+
 		showFull: function (container, src) {
-			// HERE: We got the source, now show the image.
+
+			function handleNext(evt) {
+				var targets = Drupal.behaviors.wmImagePreview.navTargets;
+				if (targets.next) {
+					targets.next.trigger('click');
+				}
+				evt.stopPropagation();
+			}
+
+			function handlePrev(evt) {
+				var targets = Drupal.behaviors.wmImagePreview.navTargets;
+				if (targets.prev) {
+					targets.prev.trigger('click');
+				}
+				evt.stopPropagation();
+			}
+
+			// Returns the overlay element. Creates it if needed.
+			function getOverlayContainer() {
+				var overlay = $('.wm-overlay-container');
+				if (overlay.length > 0) {
+					return overlay.first();
+				}
+
+				// Make the DOM object
+				var newDom = $(''
+					+ '<div class="wm-overlay-container">'
+					+ '	<div class="wm-close-icon"></div>'
+					+ '	<div class="wm-overlay-inner">'
+					+ '		<div class="wm-image-field"></div>'
+					+ '	</div>'
+					+ '	<nav><div class="wm-prev">Previous</div><div class="wm-next">Next</div></nav>'
+					+ '</div>');
+
+				// Hide the overlay on click.
+				newDom.on('click', function(evt) {
+					$(this).toggleClass('wm-visible', false);
+				});
+
+				// Handle next and previous buttons
+				newDom.find('.wm-prev').on('click', handlePrev);
+				newDom.find('.wm-next').on('click', handleNext);
+
+				return newDom.prependTo($('body'));
+			}
+
+			// Make a copy of the image field:
+			var replacement = $(container).clone();
+
+			// Replace the image with the big varsion and tell it about where it came from:
+			replacement.find('img').attr('src', src);
+			replacement.originalField = container;
+
+			var overlay = getOverlayContainer();
+
+			// Sort out the next and previous buttons:
+			var nextField = $(container).next('.wm-image-field');
+			overlay.find('.wm-next')
+				.toggleClass('wm-unavailable', nextField.length === 0);
+			Drupal.behaviors.wmImagePreview.navTargets.next = nextField.first();
+
+			var prevField = $(container).prev('.wm-image-field');
+			overlay.find('.wm-prev')
+				.toggleClass('wm-unavailable', prevField.length === 0);
+			Drupal.behaviors.wmImagePreview.navTargets.prev = prevField.first();
+
+			// Put the copied image field to the overlay and show it:
+			overlay
+				.toggleClass('wm-visible', true)
+					.find('.wm-image-field')
+					.replaceWith(replacement);
+		},
+
+		hideFull: function(container) {
+			$(container).toggleClass('wm-visible', false)
 		}
 	};
 })(jQuery);
